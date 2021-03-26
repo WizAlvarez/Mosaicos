2a3b612d2f370237189db87ab9f9d98dfccb8fb2
WizAlvarez <81273242+WizAlvarez@users.noreply.github.com>
2021-03-26 02:37:27 -0600

    Add files via upload

--- scr/features/check.js
+++ scr/features/check.js
@@ -1,32 +1,48 @@
 module.exports = async (client, message, args, Discord) => {
 
     var colors = require("colors");
-    const probe = require('probe-image-size');
+    const rango = require("../utils/range")
 
+
+    const fs = require('fs-extra');
+    const yaml = require('js-yaml');
+    let config = yaml.safeLoad(fs.readFileSync('config.yml', 'utf8'));
+    const canalbot = config.channelsIDS.botchannel
+    const prefijo = config.prefix
 
     client.on("message", (message) =>{
 
-
-        if (message.attachments.size > 0) {
-            message.attachments.forEach(Attachment => {
-
-                console.log(colors.bgGreen(`[INFO]: IMAGEN ENVIADA POR ${message.author.tag}`) + colors.yellow(` >> Url: ${Attachment.url}`))
-                message.reply(` tu imagen es: ${Attachment.url}`)
- 
-                let result = probe(`${Attachment.url}`)
-                console.log(result)
-              
-
-
-            })
-        } else {
-            //console.log(`\nCHATLOGS - [${message.guild}] ${message.author.tag}: ${message.content}`);
+        if(message.channel.id !== `${canalbot}`){
             return;
+        }else{
+
+            if(message.attachments.size > 0) {
+                message.attachments.forEach(Attachment => {
+
+                    //EJECUTA EL METODO RANGO DE LA CARPETA utils                    
+                    //REVISA SI TIENE LAS MEDIDAS CORRECTAS
+                    rango(client, message, Attachment)
+
+                    //EJECUTA EL METODO RANGO DE LA CARPETA utils                    
+                    //REVISA SI TIENE EL COLOR CORRETO
+
+                })
+
+            }else{
+                //MODO SEGURO
+                //SI ES UN COMANDO REGRESA
+                if(message.content.startsWith(`${prefijo}`)){return}
+
+            }
         }
-
-
-
-
 
     })
 }
+
+
+
+//MANDAR ESTO A COMANDO
+
+/* const probe = require('probe-image-size');
+let result = await probe(`${Attachment.url}`)
+console.log(result) */
